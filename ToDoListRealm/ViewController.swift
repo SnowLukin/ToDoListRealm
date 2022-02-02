572eadc40acdcc8a0af9d067bcf4cbd51679f544
Snow Lukin <everyoursden@gmail.com>
2022-02-02 17:40:01 +0300

    Initial Commit

---
+++ ToDoListRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListRealm
+//
+//  Created by Snow Lukin on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
